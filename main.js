ccc7395205ac23451fa8c38863bd889a2b78390c
Danielle Pinheiro <168576403+sdaniverse@users.noreply.github.com>
2026-01-20 17:55:29 -0300

    feat: implement the game logic

---
+++ main.js
@@ -0,0 +1,56 @@
+const CHOICES = 3;
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * CHOICES);
+}
+
+function getHumanChoice() {
+    return prompt("Your Choice: ");
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === "rock" && computerChoice === 1) {
+        console.log("You lose! Paper beats Rock.");
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === 2) {
+        console.log("You win! Rock beats Scissors.");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === 0) {
+        console.log("You win! Paper beats Rock.");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === 2) {
+        console.log("You lose! Scissors beats Paper.");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === 0) {
+        console.log("You lose! Rock beats Scissors.");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === 1) {
+        console.log("You win! Scissors beats Paper.");
+        humanScore++;
+    } else {
+        console.log("You tie!");
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (humanScore > computerScore)
+        console.log("You win the game!");
+    else if (computerScore > humanScore)
+        console.log("You lose the game!");
+    else
+        console.log("The game is tied!");
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+playGame();
